b771af034272297f1ed7a923cd272b20c9742c76
FrantzLacarriere <frantz.lacarriere@gmail.com>
2015-07-14 18:39:20 -0400

    added the Twit module and sent first tweet

--- server.js
+++ server.js
@@ -3,32 +3,45 @@ var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var app = express();
-
+var list = [];
+var Twit = require('twit');
+var twitInfo = require('./config.js');
+var twitter = new Twit(twitInfo);
 //This is the scraper
 
-app.get('/', function(req, res){
+twitter.post('statuses/update', {status: "Hello World"}, function (err, data, response){
+  console.log(data);
+});
 
-  //this is the r/hearthstone url that i am scraping
-  url = 'http://www.reddit.com/r/hearthstone/top/#page=1/';
+// app.get('/', function(req, res){
+//
+//   //this is the r/hearthstone url that i am scraping
+//   var url = 'https://www.reddit.com/r/hearthstone/top/?sort=top&t=day';
+//   var world = "";
+//
+//   request(url, function(error, response, html){
+//     if(!error){
+//       var $ = cheerio.load(html);
+//
+//       var title, link;
+//       var json = {title : "", link : ""};
+//
+//       //this is our entry point for data that
+//       //contains each post info
+//       $('.thing').each(function(i, elem) {
+//         list[i] = $(elem).text();
+//       });
+//
+//       console.log(list);
+//
+//     }
+//   });
+//
+//   res.send(console.log(list));
+// });
 
-  request(url, function(error, response, html){
-    if(!error){
-      var $ = cheerio.load(html);
-
-      var title, link;
-      var json = {title : "", link : ""};
-
-      //this is our entry point for data that
-      //contains each post info
-      $('p.title').filter(function(){
-
-      })
-    }
-  })
-})
-
-app.listen('9000')
+app.listen(9000);
 
 
 
-exports = module.exports = app;
+//exports = module.exports = app;
